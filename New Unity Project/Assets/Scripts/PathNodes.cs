24f03f3cf152abcd1dd7e3bc8d2bd18ef637f2e0
Cavan <macp0108>
2015-01-24 17:51:41 -0500

    Added Objects

---
+++ New Unity Project/Assets/Scripts/PathNodes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathNodes : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<MeshRenderer> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<MeshRenderer> ().enabled = false;
+	}
+}
